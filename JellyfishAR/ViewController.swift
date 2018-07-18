2e7d5b7da21f5f35273eec12b8a47521afbb4083
Evan Butler <evanb447@gmail.com>
2018-07-17 18:31:07 -0700

    Adds Jellyfish 3D model and correctly positions it in the scene

--- JellyfishAR/ViewController.swift
+++ JellyfishAR/ViewController.swift
@@ -44,10 +44,12 @@ class ViewController: UIViewController {
     }
     
     func addNode() {
-        let node = SCNNode(geometry: SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0))
-        node.position = SCNVector3(0, 0, -1)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        self.sceneView.scene.rootNode.addChildNode(node)
+        let jellyfishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyfishNode = jellyfishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
+        
+        jellyfishNode?.position = SCNVector3(0, 0, -1)
+        
+        self.sceneView.scene.rootNode.addChildNode(jellyfishNode!)
     }
     
     @objc func handleTap(sender: UITapGestureRecognizer) {

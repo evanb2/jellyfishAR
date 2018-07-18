5f04fe847d0c20c0e04c0479c7f5f890cd550d6a
Evan Butler <evanb447@gmail.com>
2018-07-18 11:16:55 -0700

    Adds countdown timer, function for reseting game, function for animating Jellyfish

--- JellyfishAR/ViewController.swift
+++ JellyfishAR/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 import ARKit
+import Each
 
 class ViewController: UIViewController {
     //MARK: Outlets
     @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var play: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
     
     //MARK: Properties
     let configuration = ARWorldTrackingConfiguration()
+    var timer = Each(1).seconds
+    var countdown = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,17 +42,30 @@ class ViewController: UIViewController {
     
     //MARK: Actions
     @IBAction func play(_ sender: Any) {
+        self.setTimer()
         self.addNode()
+        self.play.isEnabled = false
     }
     
     @IBAction func reset(_ sender: Any) {
+        self.timer.stop()
+        self.resetTimer()
+        self.play.isEnabled = true
+        self.timerLabel.text = "Let's Play!"
+        
+        sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        
+        self.sceneView.session.pause()
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
     func addNode() {
         let jellyfishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
         let jellyfishNode = jellyfishScene?.rootNode.childNode(withName: "Jellyfish", recursively: false)
         
-        jellyfishNode?.position = SCNVector3(0, 0, -1)
+        jellyfishNode?.position = SCNVector3(randomNumbers(min: -1, max: 1), randomNumbers(min: -0.5, max: 0.5), randomNumbers(min: -1, max: 1))
         
         self.sceneView.scene.rootNode.addChildNode(jellyfishNode!)
     }
@@ -57,13 +75,65 @@ class ViewController: UIViewController {
         let touchCoordinates = sender.location(in: sceneViewTappedOn)
         let hitTest = sceneViewTappedOn.hitTest(touchCoordinates)
         
+        // check if node was touched
         if hitTest.isEmpty {
             print("Didn't touch anything")
         } else {
             let result = hitTest.first!
-            let geometryInfo = result.node.geometry
-            print(geometryInfo!)
+            let node = result.node
+            
+            if countdown > 0 {
+                // only animate if animation not currently in progress
+                if node.animationKeys.isEmpty {
+                    SCNTransaction.begin()
+                    animateNode(node: node)
+                    // scene transaction ensures this code is not executed until after animation is complete
+                    SCNTransaction.completionBlock = {
+                        // removes the jellyfish we tapped on
+                        node.removeFromParentNode()
+                        // adds a new jellyfish at a random position
+                        self.addNode()
+                        self.resetTimer()
+                    }
+                    SCNTransaction.commit()
+                }
+            }
         }
+    }
+    
+    func animateNode(node: SCNNode) {
+        let spin = CABasicAnimation(keyPath: "position")
+        spin.fromValue = node.presentation.position
+        spin.toValue = SCNVector3(node.presentation.position.x - 0.2, node.presentation.position.y - 0.2, node.presentation.position.z - 0.2)
+        // duration in seconds
+        spin.duration = 0.07
+        // autoreverse animates the node back to its original position
+        spin.autoreverses = true
+        spin.repeatCount = 5
+        node.addAnimation(spin, forKey: "position")
+    }
+    
+    func randomNumbers(min firstNum: CGFloat, max secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+    
+    func setTimer() {
+        self.timer.perform { () -> NextStep in
+            self.countdown -= 1
+            self.timerLabel.text = String(self.countdown)
+            
+            if self.countdown == 0 {
+                self.timerLabel.text = "You lose!"
+                return .stop
+            }
+            
+            return .continue
+        }
+    }
+    
+    func resetTimer() {
+        self.countdown = 10
+        self.timerLabel.text = String(self.countdown)
     }
 }
 
